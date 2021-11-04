9c9dc632ad4f5858855f819b5db234642240163a
themixray <35273590+themixray@users.noreply.github.com>
2021-11-04 16:57:25 +0300

    Add files via upload

--- pygwin/__init__.py
+++ pygwin/__init__.py
@@ -1,13 +1,32 @@
-from pygwin.surface import surface
-import pygwin.keyboard as keyboard
-import pygwin.gamepad as _gp
-import pygwin.mouse as mouse
-from pygwin.rect import rect
-import pygwin.image as image
-import pygwin.mixer as mixer
-from pygame.locals import *
-import pygwin.font as font
-from pygwin._win import *
-from pygwin._pg import pg
-import pygwin.ui as ui
-gamepad = _gp.gamepad(pg)
+try:
+    from pygwin.surface import surface
+    import pygwin.keyboard as keyboard
+    import pygwin.gamepad as _gp
+    import pygwin.mouse as mouse
+    from pygwin.rect import rect
+    import pygwin.image as image
+    import pygwin.mixer as mixer
+    from pygame.locals import *
+    import pygwin.font as font
+    from pygwin._win import *
+    from pygwin._pg import pg
+    import pygwin.ui as ui
+    gamepad = _gp.gamepad(pg)
+except ModuleNotFoundError as e:
+    import pip,os,sys
+    if 'imofpgw' in sys.argv:
+        raise e
+    def install(package):
+        if hasattr(pip,'main'):pip.main(['install',package])
+        else:pip._internal.main(['install',package])
+    modules = ['datetime',
+               'tempfile',
+               'pywin32',
+               'pickle',
+               'pygame',
+               'inputs',
+               'pydub',
+               'ctypes']
+    for i in modules:
+        install(i)
+    os.execv(sys.argv[0], sys.argv+['imofpgw'])
