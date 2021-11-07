8e5bbf4a843cc8d003f54c562a2ef41648707ca4
themixray <35273590+themixray@users.noreply.github.com>
2021-11-07 11:39:07 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pygwin',
+    packages=['pygwin'],
+    version='0.0.1',
+    description='A library for creating Python applications.',
+    install_requires=['tempfile','pywin32','pickle','pygame','inputs','pydub'],
+    author='themixray',
+    license='MIT')
