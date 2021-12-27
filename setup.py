30719ab6d8607a763a606fc3825b3e6900d808a5
themixray <35273590+themixray@users.noreply.github.com>
2021-12-27 20:49:54 +0300

    3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='pgw',packages=['pygwin'],version='0.1.0',
+setup(name='pgw',packages=['pygwin'],version='0.1.2',
 author='themixray',description='A library for creating Python applications.',
 license='MIT',install_requires=['cython','pywin32','pygame','inputs','randstr',
 'pydub','wxPython','pyautogui','moviepy','pipwin','wave','opencv-python'])
